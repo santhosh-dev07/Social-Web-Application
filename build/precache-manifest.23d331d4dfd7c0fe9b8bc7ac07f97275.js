cec48f704ec36be048b1ba7af9e6108e944165d1
santhosh <santhosh.m0719@gmail.com>
2021-07-03 16:07:51 +0530

    final commit

---
+++ build/precache-manifest.23d331d4dfd7c0fe9b8bc7ac07f97275.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337d756ad0dc09b6b03b963d665070cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd5b4409cb965aa43441",
+    "url": "/static/css/main.c75658f2.chunk.css"
+  },
+  {
+    "revision": "39539cb337ef7dfa574c",
+    "url": "/static/js/2.52bcb2d8.chunk.js"
+  },
+  {
+    "revision": "c13bfd055a57809fb2d731409b713623",
+    "url": "/static/js/2.52bcb2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5b4409cb965aa43441",
+    "url": "/static/js/main.832ab576.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
